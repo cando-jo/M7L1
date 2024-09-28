a795207bc303bc64aa1ae4a808388a3d4bfe1703
Qusai GAZAWY <92310650+cando-jo@users.noreply.github.com>
2024-09-28 12:55:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 
 # Hücrelerden oluşan oyun penceresi
 hucre = Actor('sınır')
+karakter = Actor('karakter')
+
 hucre.height = 50
 hucre.width = 50
 hucre_g = 7 # Hücrenin genişliği
 hucre_h = 7 # Hücrenin yüksekliği
 WIDTH = hucre.width * hucre_g
 HEIGHT = hucre.height * hucre_h
-
 TITLE = "Zindanlar" # Oyunun Adı
 FPS = 30 # Saniyedeki Kare Sayısı
 
@@ -20,9 +21,15 @@ haritam = [[0, 0, 0, 0, 0, 0, 0],
            [0, 1, 3, 1, 1, 2, 0],
            [0, 0, 0, 0, 0, 0, 0]]
 
+karakter.health = 100
+karakter.attack = 5
+
 def draw():
     harita_cizim()
-
+    screen.draw.text(karakter.health, center=(325, 10), color = 'white', fontsize = 16)
+    screen.draw.text(karakter.attack, center=(325, 25), color = 'white', fontsize = 16)
+    karakter.draw()
+    
 def harita_cizim():
     for i in  range ( len ( haritam ) ) :
         for j in  range ( len ( haritam [ i ] ) ) : 
